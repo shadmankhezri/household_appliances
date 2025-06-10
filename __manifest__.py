b972d3b6b66f0c58a9c9cf8f5cbdfc3436ab4711
shadman_git <khezri.shadman@gmail.com>
2025-06-10 11:48:20 +0330

    create first structer

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Household Appliances',
+    'version': '17.0.1.0.0',
+    'category': 'Sales',
+    'summary': 'Online store for household appliances with brand and stock management',
+    'author': 'Your Name',
+    'website': 'https://github.com/username/household_appliances',
+    'depends': ['base', 'product', 'sale_management', 'stock', 'account', 'contacts'],
+    'data': [
+        # Add your XML/CSV files here
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
