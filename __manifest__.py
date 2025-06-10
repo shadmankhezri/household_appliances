53aec2fbc4cf674263f0684527f008a4a4b878e5
shadman_git <khezri.shadman@gmail.com>
2025-06-10 12:02:47 +0330

    update structer

--- __manifest__.py
+++ __manifest__.py
@@ -3,13 +3,13 @@
     'version': '17.0.1.0.0',
     'category': 'Sales',
     'summary': 'Online store for household appliances with brand and stock management',
-    'author': 'Your Name',
-    'website': 'https://github.com/username/household_appliances',
-    'depends': ['base', 'product', 'sale_management', 'stock', 'account', 'contacts'],
+    'author': 'Shadman Khezri',
+    # 'depends': ['base', 'product', 'sale_management', 'stock', 'account', 'contacts'],
+    'depends': ['base'],
     'data': [
         # Add your XML/CSV files here
     ],
     'installable': True,
     'application': True,
-    'license': 'LGPL-3',
+    # 'license': 'LGPL-3',
 }
